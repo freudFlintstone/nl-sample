9a928d1ce9455e9e8dbf1c9a9dc814f274517c6e
freudFlintstone <raphael.mattos@contextaware.com.br>
2021-01-17 17:16:20 -0300

    main drawer top section

---
+++ second-task/src/elements/user-profile.js
@@ -0,0 +1,69 @@
+import {PolymerElement, html} from '@polymer/polymer/polymer-element.js';
+import '../shared-styles.js';
+
+/**
+ * `user-profile` Description
+ *
+ * @customElement
+ * @polymer
+ * @demo
+ * 
+ */
+class UserProfile extends PolymerElement {
+  static get properties() {
+    return {
+      userName: {
+        type: String,
+        value: ''
+      }
+    };
+  }
+
+  static get template() {
+    return html`
+      <style include="shared-styles">
+        :host {
+          color: white;
+        }
+
+        .avatar {
+          display: block;
+          margin-top: 20px;
+        }
+
+        .avatar iron-icon{
+          --iron-icon-height: 48px;
+          --iron-icon-width: 48px;
+        }
+
+        paper-item {
+          font-size: 0.9em;
+        }
+      </style>
+      <div class="circle avatar"><iron-icon icon="account-circle"></iron-icon></div>
+      <paper-item bottom-item>
+        Welcome back, [[userName]]
+        <paper-icon-button icon="arrow-drop-down"></paper-icon-button>
+      </paper-item>
+    `;
+  }
+
+  /**
+   * Instance of the element is created/upgraded. Use: initializing state,
+   * set up event listeners, create shadow dom.
+   * @constructor
+   */
+  constructor() {
+    super();
+  }
+
+  /**
+   * Use for one-time configuration of your component after local
+   * DOM is initialized.
+   */
+  ready() {
+    super.ready();
+  }
+}
+
+customElements.define('user-profile', UserProfile);
